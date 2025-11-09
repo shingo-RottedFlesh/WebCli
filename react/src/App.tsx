51f78087aba87d3960236ac0d0bfca959a6bb02d
shingo-RottedFlesh <shingo.rottedflesh@gmail.com>
2025-11-10 01:36:32 +0900

    ログイン画面作成（画面遷移をどうするか＆バックエンド側への連携はまだ）

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -3,21 +3,23 @@ import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 import Page1 from './sub/Page1.tsx'
 import Page2 from './sub/Page2.tsx'
 import Home from './sub/Home.tsx'
+import Login from './login/LoginForm.tsx'
 
 function App() {
   return (
     <>
-      
       <BrowserRouter>
         {/* <Link to="/">App</Link><br/> */}
         <Link to="/home">Home</Link><br/>
         <Link to="/page1">Page1</Link><br/>
         <Link to="/page2">Page2</Link><br/>
+        <Link to="/login">login</Link><br/>
         <Routes>
           {/* <Route path="/" element={<App />} /> */}
           <Route path="/home" element={<Home />} />
           <Route path="/page1" element={<Page1 />} />
           <Route path="/page2" element={<Page2 />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ react/src/login/LoginForm.tsx
@@ -0,0 +1,54 @@
+import {useState, useRef, type FormEvent} from "react"
+import { useNavigate } from 'react-router-dom';
+
+const LoginForm = () => {
+    // ログインの入力項目の変数設定
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    // バリデーション部分の要素操作用
+    const validationRef = useRef<HTMLDivElement>(null);
+
+    const navigate = useNavigate();
+
+    const submitForm = (event: FormEvent) => {
+        // ページのリロードを防ぐ
+        event.preventDefault();
+        
+        // Todo：Golang側のログイン認証ロジックを使う
+
+        if (username === 'shingo' && password === 'password' ){
+            navigate('/home')
+        } else {
+            if (validationRef.current) {
+                validationRef.current.innerText = 'ログインに失敗しました';
+            } else {
+                alert("alidationRef.currentがnullです");
+            }
+        }
+    }
+
+
+    return(
+        <>
+        <div>ログイン画面</div>
+        <form onSubmit={submitForm}>
+            <div>
+                <div id='validation' 
+                    ref={validationRef}
+                    style={{ color: '#ff0000'}}></div>
+                <label htmlFor="username">ユーザ名</label>
+                <input type="text" id="username" value={username} 
+                    onChange={(e) => setUsername(e.target.value)} required />
+                <br/>
+                <label htmlFor="password">パスワード</label>
+                <input type="password" id="password" value={password} 
+                    onChange={(e) => setPassword(e.target.value)} required />
+            </div>
+            <input type="submit" />
+        </form>
+        </>
+    );
+}
+
+
+export default LoginForm
