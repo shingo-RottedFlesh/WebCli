9a153c03126d2fb79f77bef0333a84a44cd8c788
shingo-RottedFlesh <shingo.rottedflesh@gmail.com>
2025-11-13 01:53:44 +0900

    ルートパスをログイン画面に変更

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,29 +1,25 @@
 import './App.css'
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 import Page1 from './sub/Page1.tsx'
 import Page2 from './sub/Page2.tsx'
 import Home from './sub/Home.tsx'
 import Login from './login/LoginForm.tsx'
+import React from 'react';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <>
-      <BrowserRouter>
-        {/* <Link to="/">App</Link><br/> */}
-        <Link to="/home">Home</Link><br/>
-        <Link to="/page1">Page1</Link><br/>
-        <Link to="/page2">Page2</Link><br/>
-        <Link to="/login">login</Link><br/>
-        <Routes>
-          {/* <Route path="/" element={<App />} /> */}
-          <Route path="/home" element={<Home />} />
-          <Route path="/page1" element={<Page1 />} />
-          <Route path="/page2" element={<Page2 />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <div>
+      <Routes>
+        {/* <Route path="/" element={<App />} /> */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
+
+
 
 export default App
