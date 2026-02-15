8ee83f6fc6903c393d178f49c810f9ab17f5ea8d
shingo-RottedFlesh <shingo.rottedflesh@gmail.com>
2026-02-15 15:08:12 +0900

    コメント追加

--- react/src/components/RequireAuth.tsx
+++ react/src/components/RequireAuth.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 
+/* 
+ *  このコンポーネントは、ログインしているかどうかを判定し、
+ *  ログインしていない場合はログインページにリダイレクトする
+ *  ログインしている場合は、children要素を表示する
+ */
+// 引数で渡されるprops の children要素を受け取る
 function RequireAuth({ children }: { children: React.ReactNode }) {
     const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
     const location = useLocation();
